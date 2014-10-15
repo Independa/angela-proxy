3e4f850a9942cfe608fd50aa88a294b7c83367be
Peter Hsieh <peter@tailwindweb.com>
2014-10-15 15:11:24 -0700

    Allow CORS

--- web.js
+++ web.js
@@ -5,6 +5,26 @@ var FB = require('fb');
 
 app.use(logfmt.requestLogger());
 
+// Enables CORS
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
+// enable CORS!
+app.use(enableCORS);
+
+
 app.get('/', function(req, res){
   res.send("Independa Proxy");
 });
