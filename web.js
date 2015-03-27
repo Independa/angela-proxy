ffe2988fa1c10e84bdce73d551a417a17693b8c0
wenpeng zou <wen@tailwindweb.com>
2015-03-27 14:37:33 -0700

    comment out allow any origin

--- web.js
+++ web.js
@@ -4,12 +4,12 @@ var app = express();
 var FB = require('./fb/fb.js');
 
 app.use(logfmt.requestLogger());
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
-});
+});*/
 app.get('/', function(req, res){
   res.send("Independa Proxy");
 });
